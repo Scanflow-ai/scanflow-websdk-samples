62304b9805c0988b63668fe1a24809a2e634a3b5
Ramaswamy TN <ramaswamy.tn@optisolbusiness.com>
2022-10-11 21:11:13 +0530

    Code Updated

--- src/App.js
+++ src/App.js
@@ -1,160 +1,34 @@
 /* eslint-disable react/jsx-no-target-blank */
-import React, { useEffect, useState } from "react";
-import * as ScanflowSDK from "scanflow_websdk_demo";
-import environment from "./environment/environment";
-import Container from "@mui/material/Container";
-import SFIDCapture from "./SFIDCapture";
-import SFSideBar from "./SFSideBar";
+import React from "react";
 import {
-  FormControl,
-  MenuItem,
-  Select,
-  Toolbar,
-  Typography,
-  Box,
-  AppBar,
-} from "@mui/material";
-
-const licenseKey = environment.LICENSE_KEY;
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from "react-router-dom";
+import Home from "./Home";
 const App = () => {
-  const [devices, setDevices] = useState([]);
-  const [deviceId, setDeviceId] = React.useState("");
-  const [rerender, setRerender] = useState(false);
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    // Created Package with Test License Key
-    ScanflowSDK.configureSDK(licenseKey)
-      .then(async (res) => {
-        const rootElement = document.getElementById("camera_view");
-        const captureObj = new ScanflowSDK.CaptureView(rootElement);
-        const devices = await captureObj.getMediaDevices();
-        setDevices([...devices]);
-        const eventEmitter = new ScanflowSDK.IDCapture().addListener();
-        eventEmitter.on("getData", (data) => {
-          setData([data]);
-          handleClickOpen();
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [rerender]);
-  const changeDevices = (event) => {
-    const constraints = {
-      deviceId: event.target.value,
-    };
-    console.log(constraints);
-    const rootElement = document.getElementById("camera_view");
-    new ScanflowSDK.CaptureView(rootElement, constraints);
-  };
-  const [open, setOpen] = React.useState(false);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const reload = () => window.location.reload();
   return (
-    <Container maxWidth="xl">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="fixed" className="appBar">
-          <Toolbar disableGutters>
-            <Box
-              sx={{
-                ml: 4,
-                display: { xs: "flex", md: "flex" },
-                alignItems: { xl: "flex-start" },
-              }}
-            >
-              <img
-                src={
-                  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Video_camera_icon.svg/2048px-Video_camera_icon.svg.png"
-                }
-                height={50}
-                width={50}
-                alt={"camera"}
-              />
-              {devices ? (
-                <FormControl
-                  sx={{ ml: 1, width: "fit-content", textAlign: "center" }}
-                >
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    label="Select Camera Type"
-                    onChange={(event) => changeDevices(event)}
-                    value={deviceId}
-                    onClick={() => setRerender(true)}
-                    sx={{ textAlign: "center" }}
-                  >
-                    {devices.map((e, index) => (
-                      <MenuItem value={e.deviceId} key={index}>
-                        {e.label}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              ) : null}
-            </Box>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
 
-            <Box
-              sx={{ flexGrow: 5, display: { xs: "none", md: "flex" } }}
-            ></Box>
-            <Box
-              className="d-block m-auto m-auto"
-              sx={{
-                flexGrow: 1,
-                display: { xs: "flex", md: "flex" },
-                textAlign: "center",
-              }}
-            >
-              <Typography
-                variant="h6"
-                sx={{
-                  display: "inline",
-                  fontWeight: "bolder",
-                  color: "white",
-                  textDecoration: "none",
-                  textAlign: "center",
+          <Route exact path="/docs" />
+          <Route
+            path="/docs"
+            render={() => (
+              <Navigate
+                to={{
+                  pathname: "https://demo.scanflow.ai/docs/",
                 }}
-              >
-                AadharCard Capture
-              </Typography>
-            </Box>
-            <Box
-              sx={{ flexGrow: 5, display: { xs: "none", md: "flex" } }}
-            ></Box>
-
-            <Box sx={{ flexGrow: 0, mr: 4 }}>
-              <Typography
-                variant="h6"
-                color="white"
-                className="d-inline nav-link"
-              >
-                <a
-                  href={
-                    "https://github.com/Scanflow-ai/scanflow-websdk-samples"
-                  }
-                  target="_blank"
-                  className="nav-link"
-                >
-                  Demo Code
-                </a>
-              </Typography>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <SFIDCapture />;
-      <SFSideBar
-        handleClose={handleClose}
-        handleClickOpen={handleClickOpen}
-        open={open}
-        data={data}
-      />
-    </Container>
+              />
+            )}
+          />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
--- src/SFSideBar.js
+++ src/SFSideBar.js
@@ -14,49 +14,70 @@ export default function SFResultDialog(props) {
       >
         <Modal.Header closeButton>Aadhar Card Details</Modal.Header>
         <Modal.Body>
-          <img
-            className="d-block m-auto m-auto"
-            src={`data:image/jpeg;base64,${data[0]?.image}`}
-            width={200}
-            height={200}
-            alt={data[0]?.name}
-          />
+          {data[0]?.image && (
+            <img
+              className="d-block m-auto m-auto"
+              src={`data:image/jpeg;base64,${data[0]?.image}`}
+              width={200}
+              height={200}
+              alt={data[0]?.name}
+            />
+          )}
           <Container>
-            <Row>
-              <Col>Name</Col>
-              <Col xs={2} className="text-center">
-                -
-              </Col>
-              <Col xs={6}>{data[0]?.name}</Col>
-            </Row>
-            <Row>
-              <Col>DOB</Col>
-              <Col xs={2} className="text-center">
-                -
-              </Col>
-              <Col xs={6}>{data[0]?.dob}</Col>
-            </Row>
-            <Row>
-              <Col>Gender</Col>
-              <Col xs={2} className="text-center">
-                -
-              </Col>
-              <Col xs={6}>{data[0]?.sex}</Col>
-            </Row>
-            <Row>
-              <Col>Aadhar Id</Col>
-              <Col xs={2} className="text-center">
-                -
-              </Col>
-              <Col xs={6}>{data[0]?.uniqueId}</Col>
-            </Row>
-            <Row>
-              <Col>Address</Col>
-              <Col xs={2} className="text-center">
-                -
-              </Col>
-              <Col xs={6}>{data[0]?.address}</Col>
-            </Row>
+            {data[0]?.name && (
+              <Row>
+                <Col>Name</Col>
+                <Col xs={2} className="text-center">
+                  -
+                </Col>
+                <Col xs={6}>{data[0]?.name}</Col>
+              </Row>
+            )}
+            {data[0]?.fatherName && (
+              <Row>
+                <Col>Father Name</Col>
+                <Col xs={2} className="text-center">
+                  -
+                </Col>
+                <Col xs={6}>{data[0]?.fatherName}</Col>
+              </Row>
+            )}
+            {data[0]?.dob && (
+              <Row>
+                <Col>DOB</Col>
+                <Col xs={2} className="text-center">
+                  -
+                </Col>
+                <Col xs={6}>{data[0]?.dob}</Col>
+              </Row>
+            )}
+            {data[0]?.sex && (
+              <Row>
+                <Col>Gender</Col>
+                <Col xs={2} className="text-center">
+                  -
+                </Col>
+                <Col xs={6}>{data[0]?.sex}</Col>
+              </Row>
+            )}
+            {data[0]?.uniqueId && (
+              <Row>
+                <Col>Aadhar Id</Col>
+                <Col xs={2} className="text-center">
+                  -
+                </Col>
+                <Col xs={6}>{data[0]?.uniqueId}</Col>
+              </Row>
+            )}
+            {data[0]?.address && (
+              <Row>
+                <Col>Address</Col>
+                <Col xs={2} className="text-center">
+                  -
+                </Col>
+                <Col xs={6}>{data[0]?.address}</Col>
+              </Row>
+            )}
           </Container>
         </Modal.Body>
       </Modal>
